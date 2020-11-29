e024b4d758a300fbb38c4efc564c23fef8bb016d
Sangha Yoon <yoons4@spu.edu>
2020-11-29 19:42:08 +0900

    Add cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
